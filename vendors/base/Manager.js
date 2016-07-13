65ffd79f0c83fbcf9da3a8d6f87eb1d4cc31f429
Yann Amsellem <yann_ams@icloud.com>
2016-07-13 12:22:21 +0000

    Manager.js edited online with Bitbucket

--- vendors/base/Manager.js
+++ vendors/base/Manager.js
@@ -6,7 +6,11 @@
             this.remove = {};
             this.get    = {};
             this._name = 'base_manager_name';
+            
+            this.init();
         }
+        
+        init() {}
     }
 
 /*Exports*/

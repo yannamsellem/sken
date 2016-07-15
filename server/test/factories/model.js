5a30eedb436bde87b8f154a71ee736937bb49964
Yann Amsellem <yann_ams@icloud.com>
2016-07-15 14:47:51 +0200

    test improve, drivers updated (mongo and sequelize)

---
+++ server/test/factories/model.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  let Model = sequelize.define('Model',
+  {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+  }, {
+    classMethods: {
+        associate: (models) => {}
+    }
+  });
+
+  return Model;
+};

ea56cb5d8565d689206136e61b466d6310ed3d90
Yann Amsellem <yann_ams@icloud.com>
2016-09-08 09:44:49 +0200

    check parent module before stating the server (for test matters)

--- app/app.js
+++ app/app.js
@@ -9,12 +9,14 @@ require(__dirname + '/constantLoader').init();
 require(__dirname + '/configLoader').init();
 require(__dirname + '/appKernel').init(app, server);
 
-server.listen(config.server.port, config.server.address, function() {
-	debug('%s application is listening on %s:%d - [%s]',
-		process.env.npm_package_name,
-		this.address().address,
-		this.address().port,
-		process.env.npm_package_author_name);
-});
+if (!module.parent) {
+	server.listen(config.server.port, config.server.address, function() {
+		debug('%s application is listening on %s:%d - [%s]',
+			process.env.npm_package_name,
+			this.address().address,
+			this.address().port,
+			process.env.npm_package_author_name);
+	});
+}
 
 module.exports = { app: app, server: server };

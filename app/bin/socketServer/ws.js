2af398348c5e4ce5f9a5543a09f5fe97664af7d5
Yann Amsellem <yann_ams@icloud.com>
2016-09-12 17:26:48 +0200

    ws support

---
+++ app/bin/socketServer/ws.js
@@ -0,0 +1,31 @@
+/*Services requiring*/
+	const { Server } = require('ws');
+
+/*Bins requiering*/
+
+	const Module 	= require( global.paths.bin + '/module');
+
+/*Variables declaration*/
+		var sockets = null;
+
+/*Class declaration*/
+
+	class WebSocketServer {
+		constructor() {}
+
+		static init (app, server) {
+			sockets = new Server({server: server, path: config.websocket.client_path});
+
+			sockets.broadcast = (data) => sockets.clients.forEach((client) => client.send(data));
+
+			sockets.on('connection', (socket) => Module.socketInit(sockets, socket));
+			return this;
+		}
+
+		static get() {
+			return sockets;
+		}
+	}
+
+/*Exports*/
+module.exports = WebSocketServer;

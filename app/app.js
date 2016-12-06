dd6ab51a3f9c144fcb986a4c48627ac71b099422
Yann Amsellem <yann_ams@icloud.com>
2016-12-06 15:02:25 +0100

    yarn added

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ require(join(__dirname, '/configLoader')).init();
 require(join(__dirname, '/appKernel')).init(app, server);
 
 if (!module.parent) {
-  server.listen(config.server.port, config.server.address, () => {
+  server.listen(config.server.port, config.server.address, function () {
     debug('%s application is listening on %s:%d - [%s]', process.env.npm_package_name, this.address().address, this.address().port, process.env.npm_package_author_name);
   });
 }
